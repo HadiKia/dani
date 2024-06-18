17cc5df457dafed709d8633a3929577103dea5ef
HadiKia <hadikiamarzi@protonmail.com>
2024-06-19 00:47:24 +0330

    header was created

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans : ["iransans"],
+      },
+      colors: {
+        primary: "#132662",
+        secondary: "#FEB41C",
+        yellowLight: "#FCD34D",
+        yellowLight4: "#FFFBEB",
+        textColor : "#040816"
+      },
+    },
+  },
+  plugins: [],
+};
